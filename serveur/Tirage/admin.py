6e123ed562170da0aca1d1347532cd03da7e8fcb
root <root@Soraya-Yoga-Lenovo>
2024-11-04 15:46:34 +0100

    added models: Pays, equipe, chapeau, championnat

---
+++ serveur/Tirage/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from Tirage.models import *
+# Register your models here.
+
+
+admin.site.register(Pays)
+admin.site.register(Championnat)
+admin.site.register(Equipe)
+admin.site.register(Chapeau)
+
---
+++ serveur/Tirage/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+class NameModel(models.Model):
+    nom = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nom
+
+class Pays(NameModel):
+    pass
+
+class Championnat(NameModel):
+    pass
+
+class Chapeau(models.Model):
+    numero_de_chapeau = models.IntegerField()
+
+class Equipe(NameModel):
+    quel_pays = models.ForeignKey(Pays, on_delete=models.CASCADE)
+    quel_championat = models.ForeignKey(Championnat, on_delete=models.CASCADE)
+    quel_chapeau = models.ForeignKey(Chapeau, on_delete=models.CASCADE)
+    logo = models.ImageField()
