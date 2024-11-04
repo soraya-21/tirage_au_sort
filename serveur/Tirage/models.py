8b1b4ed7fad34a85a20740d8f403e4bedf8f3b7b
root <root@Soraya-Yoga-Lenovo>
2024-11-04 16:38:29 +0100

    loaded teams in db

--- serveur/Tirage/models.py
+++ serveur/Tirage/models.py
@@ -16,9 +16,11 @@ class Championnat(NameModel):
 class Chapeau(models.Model):
     numero_de_chapeau = models.IntegerField()
 
+    def __str__(self):
+        return self.numero_de_chapeau
+
 class Equipe(NameModel):
     quel_pays = models.ForeignKey(Pays, on_delete=models.CASCADE)
     quel_championat = models.ForeignKey(Championnat, on_delete=models.CASCADE)
     quel_chapeau = models.ForeignKey(Chapeau, on_delete=models.CASCADE)
     logo = models.ImageField()
-    
--- serveur/Tirage/views.py
+++ serveur/Tirage/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from Tirage.models import *
 
 # Create your views here.
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = tickets.objects.all()
+
